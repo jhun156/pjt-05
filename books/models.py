4bbaec7d262fd4bbdf4574aec4771e4588fe2516
박지훈 <qkrwlgns156@naver.com>
2025-04-18 13:09:11 +0900

    books.model

--- books/models.py
+++ books/models.py
@@ -7,3 +7,8 @@ class Book(models.Model):
     customer_review_rank=models.IntegerField()
     author=models.CharField(max_length=100)
     cover_image=models.ImageField(blank=True)
+
+    author_profile_img = models.ImageField(black=True)
+    author_info = models.TextField()
+    author_works = models.TextField()
+    audio_file = models.FileField(blank=True, upload_to='audio/')
