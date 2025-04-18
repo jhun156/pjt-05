204762d1ea52071b615cbdd67fb8e7c9dce470a1
박지훈 <qkrwlgns156@naver.com>
2025-04-18 14:19:02 +0900

    book view 수정

--- books/forms.py
+++ books/forms.py
@@ -1,13 +1,18 @@
 from django import forms
-from .models import Book
-
-
+from .models import Book, Thread
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = "__all__"
+        fields = '__all__'
+        widgets = {
+            'reading_date': forms.DateInput(attrs={'type': 'date'}),
+        }
 
-
-
-
+class ThreadForm(forms.ModelForm):
+    class Meta:
+        model = Thread
+        fields = '__all__'
+        widgets = {
+            'reading_date': forms.DateInput(attrs={'type': 'date'}),
+        }
